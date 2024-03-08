5764e81993e68f14a98efc0aed22b1faad63edfd
Alik <olegsobolev@Noutbuk-Oleg.local>
2024-03-08 19:15:53 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	444.py

--- my_telebot1.py
+++ my_telebot1.py
@@ -13,9 +13,9 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # URL веб-сайта, номер телефона, ссылка на прайс-лист и адрес офисов
-website_url = 'http://www.translit-nvrsk.ru/ru'
+website_url = 'http://www.translit-nvrsk.ru'
 phone_number = '+79883232525'
-price_list_url = 'http://www.translit-nvrsk.ru/price_list'
+price_list_url = 'http://www.translit-nvrsk.ru/ru/ct-menu-item-3'
 office_address = 'г. Новороссийск, ул. Советов, 42 (БЦ "Черноморский"), офис №1,. г. Новороссийск, ул. Мира, 37 (рядом с нотариальной конторой)'
 
 # Создаем клавиатуру главного меню
