e79fdc589d62f7efafceeb59a0c85dec13cb9b74
Alik <olegsobolev@Noutbuk-Oleg.local>
2024-03-09 18:38:20 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	444.py

--- my_telebot1.py
+++ my_telebot1.py
@@ -1,7 +1,8 @@
-import telebot
-from telebot import types
-from dotenv import load_dotenv
 import os
+import telebot
+from dotenv import load_dotenv
+from googletrans import Translator
+from telebot import types
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -22,6 +23,8 @@ office_address = 'г. Новороссийск, ул. Советов, 42 (БЦ "
 main_menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 main_menu_markup.row(types.KeyboardButton("Ссылка на сайт"), types.KeyboardButton("Звонок на номер"))
 main_menu_markup.row(types.KeyboardButton("Прайс услуг"), types.KeyboardButton("Адрес офисов"))
+main_menu_markup.row(types.KeyboardButton("Перевести с русского на английский"), types.KeyboardButton("Перевести с английского на русский"))
+
 
 # Обработчик команды /start и /help
 @bot.message_handler(commands=['start', 'help'])
@@ -31,30 +34,65 @@ def send_welcome(message):
     bot.reply_to(message, welcome_message, reply_markup=main_menu_markup)
     send_main_menu(message)
 
+
 # Обработчик кнопок
 @bot.message_handler(func=lambda message: message.text == "Ссылка на сайт")
 def send_website_link(message):
     bot.reply_to(message, f"Вы можете посетить наш веб-сайт здесь: {website_url}")
     send_main_menu(message)
 
+
 @bot.message_handler(func=lambda message: message.text == "Прайс услуг")
 def send_price_list(message):
     bot.reply_to(message, f"Вы можете посмотреть наш прайс услуг по ссылке: {price_list_url}")
     send_main_menu(message)
+
 
 @bot.message_handler(func=lambda message: message.text == "Адрес офисов")
 def send_office_address(message):
     bot.reply_to(message, f"Адрес наших офисов: {office_address}")
     send_main_menu(message)
 
+
 @bot.message_handler(func=lambda message: message.text == "Звонок на номер")
 def make_phone_call(message):
     bot.send_contact(message.chat.id, phone_number)
     send_main_menu(message)
 
+
+@bot.message_handler(func=lambda message: message.text == "Перевести с русского на английский")
+def translate_to_english(message):
+    bot.reply_to(message, "Введите текст для перевода с русского на английский.")
+    bot.register_next_step_handler(message, process_translation_to_english)
+    send_main_menu(message)
+
+@bot.message_handler(func=lambda message: message.text == "Перевести с английского на русский")
+def translate_to_russian(message):
+    bot.reply_to(message, "Введите текст для перевода с английского на русский.")
+    bot.register_next_step_handler(message, process_translation_to_russian)
+    send_main_menu(message)
+
+
+def process_translation_to_english(message):
+    try:
+        translated_text = translate_text(message.text, 'ru', 'en')
+        bot.reply_to(message, f"Перевод с русского на английский: {translated_text}")
+    except Exception as e:
+        bot.reply_to(message, "Произошла ошибка при переводе.")
+
+
+def process_translation_to_russian(message):
+    try:
+        translated_text = translate_text(message.text, 'en', 'ru')
+        bot.reply_to(message, f"Перевод с английского на русский: {translated_text}")
+    except Exception as e:
+        bot.reply_to(message, "Произошла ошибка при переводе")
+
+
 # Функция для отправки главного меню
 def send_main_menu(message):
     bot.send_message(message.chat.id, "Выберите действие из меню.", reply_markup=main_menu_markup)
+
 
 # Главный цикл обработки сообщений
 bot.polling(none_stop=True)
