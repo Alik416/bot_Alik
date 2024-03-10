3aa3968cceb9235ae7b754eea450acba186d1b7b
Alik <olegsobolev@Noutbuk-Oleg.local>
2024-03-10 20:22:07 +0700

    Merge remote-tracking branch 'origin/main'
    v1.0

--- translit_bot.py
+++ translit_bot.py
@@ -2,7 +2,7 @@ import os
 import telebot
 from dotenv import load_dotenv
 from telebot import types
-from googletrans import Translator
+
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -23,7 +23,6 @@ office_address = 'г. Новороссийск, ул. Советов, 42 (БЦ "
 main_menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 main_menu_markup.row(types.KeyboardButton("Ссылка на сайт"), types.KeyboardButton("Звонок на номер"))
 main_menu_markup.row(types.KeyboardButton("Прайс услуг"), types.KeyboardButton("Адрес офисов"))
-main_menu_markup.row(types.KeyboardButton("Перевести с русского на английский"), types.KeyboardButton("Перевести с английского на русский"))
 
 
 # Обработчик команды /start и /help
@@ -56,47 +55,13 @@ def send_office_address(message):
 
 @bot.message_handler(func=lambda message: message.text == "Звонок на номер")
 def make_phone_call(message):
-    bot.send_contact(message.chat.id, phone_number,"Translit Office")
-    send_main_menu(message)
-
-
-@bot.message_handler(func=lambda message: message.text == "Перевести с русского на английский")
-def translate_to_english(message):
-    bot.reply_to(message, "Введите текст для перевода с русского на английский.")
-    bot.register_next_step_handler(message, process_translation_to_english)
-
-
-@bot.message_handler(func=lambda message: message.text == "Перевести с английского на русский")
-def translate_to_russian(message):
-    bot.reply_to(message, "Введите текст для перевода с английского на русский.")
-    bot.register_next_step_handler(message, process_translation_to_russian)
-
-
-def process_translation_to_english(message):
-    try:
-        translated_text = translate_text(message.text, 'ru', 'en')
-        bot.send_message(message.chat.id, f"Перевод с русского на английский: {translated_text}")
-    except Exception as e:
-        bot.reply_to(message, "Произошла ошибка при переводе.")
-    send_main_menu(message)
-
-def process_translation_to_russian(message):
-    try:
-        translated_text = translate_text(message.text, 'en', 'ru')
-        bot.reply_to(message.chat.id, f"Перевод с английского на русский: {translated_text}")
-    except Exception as e:
-        bot.reply_to(message, "Произошла ошибка при переводе")
+    bot.send_contact(message.chat.id, phone_number,"Советов 42 офис № 1")
     send_main_menu(message)
 
 # Функция для отправки главного меню
 def send_main_menu(message):
     bot.send_message(message.chat.id, "Выберите действие из меню.", reply_markup=main_menu_markup)
 
-
-def translate_text(text, source_lang, dest_lang):
-    translator = Translator()
-    translated = translator.translate(text, src=source_lang, dest=dest_lang)
-    return translated.text
 
 # Главный цикл обработки сообщений
 bot.polling(none_stop=True)
