cc6458b12126c3cb06335d96d520db15029dfdf3
Олег Соболев <olegsobolev@Noutbuk-Oleg.local>
2024-03-14 22:06:37 +0700

    Committing changes before switching branches

--- translit_bot.py
+++ translit_bot.py
@@ -1,14 +1,14 @@
 import os
+
 import telebot
 from dotenv import load_dotenv
 from telebot import types
 
 # Загрузка переменных окружения из файла .env
-load_dotenv()
+load_dotenv('. evn')
 
 # Получение токена бота из переменных окружения
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
 # Инициализация бота
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -21,7 +21,7 @@ office_address = 'г. Новороссийск, ул. Советов, 42 (БЦ "
 main_menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 main_menu_markup.row(types.KeyboardButton("Ссылка на сайт"), types.KeyboardButton("Наши Контакты"))
 main_menu_markup.row(types.KeyboardButton("Прайс услуг"), types.KeyboardButton("Адрес офисов"),
-                     types.KeyboardButton("Стоимость паспортов"))
+                     types.KeyboardButton("Стоимость паспортов"), types.KeyboardButton("Апостил-Консульская легализация" ))
 
 
 # Обработчик команды /start и /help
@@ -36,12 +36,32 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: message.text == "Стоимость паспортов")
 def send_passport_price(message):
     passport_price_text = """Стоимость паспортов:
-    - Перевод паспорта РФ - 1000 руб.
-    - Перевод с бирдянского"""
+    - Перевод паспартов - 1200 руб.
+    - Перевод с бередянского"""
     bot.reply_to(message, passport_price_text)
     send_main_menu(message)
 
+
 # Обработчик кнопок
+@bot.message_handler(func=lambda message:message.text == "Апостил-Консульская легализация")
+def send_legalization_info(message):
+    legalization_info = "Консульская легализация - это один из способов легализации документов при внешнеэкономической деятельности. " \
+                        "Аналогичен апостилю. В отличие от апостиля применяется при документообороте с организациями, происходящими из стран, " \
+                        "не являющихся участницами Гаагской конвенции об отмене требований легализации иностранных официальных документов. " \
+                        "По сравнению с апостилем, является более сложной двусторонней процедурой."
+    bot.reply_to(message, legalization_info)
+
+#текстовый фаил с странами список
+    with open('legalization_document.txt', 'rb') as document_file:
+        bot.send_document(message.chat.id, document_file)
+
+#фото пример доков
+    with open('example_document.jpg', 'rb') as photo_file:
+        bot.send_photo(message.chat.id, photo_file)
+    send_main_menu(message)
+
+
+
 @bot.message_handler(func=lambda message: message.text == "Ссылка на сайт")
 def send_website_link(message):
     bot.reply_to(message, f"Вы можете посетить наш веб-сайт здесь: {website_url}")
@@ -66,9 +86,9 @@ def send_office_address(message):
 
 @bot.message_handler(func=lambda message: message.text == "Наши Контакты")
 def send_contacts(message):
-    bot.send_message(message.chat.id, "Контакты офиса на улице Советов (офис №1):\n+79881313424",
+    bot.send_message(message.chat.id, "Контакты офиса на улице Советов 42 (офис №1):\n+79628555915",
                      reply_markup=main_menu_markup)
-    bot.send_message(message.chat.id, "Контакты офиса на улице Мира (рядом с нотариальной конторой):\n+79883232525",
+    bot.send_message(message.chat.id, "Контакты офиса на улице Мира 37 (рядом с нотариальной конторой):\n+79883232525",
                      reply_markup=main_menu_markup)
 
 
@@ -78,4 +98,5 @@ def send_main_menu(message):
 
 
 # Главный цикл обработки сообщений
+
 bot.polling(none_stop=True)
